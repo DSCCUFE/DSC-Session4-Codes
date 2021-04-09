98780ee6a7395bf74de7845539071c275ff3bf19
Amr Mohamed Zaki <amrzaki2000.az@gmail.com>
2021-04-09 22:00:45 +0200

    Session 4 Codes

---
+++ dsc_session/android/app/src/main/kotlin/com/example/dsc_session/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dsc_session
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ dsc_session/lib/main.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: MySplash(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+
+    );
+  }
+}
+
+class MySplash extends StatefulWidget {
+  MySplash({Key key, this.title}) : super(key: key);
+
+
+  final String title;
+
+  @override
+  MySplashState createState() => MySplashState();
+}
+
+class MySplashState extends State<MySplash> {
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+
+      ),
+      body: SplashScreen(
+        seconds: 10,
+        image: Image.asset('images/macicon.png'),
+        photoSize: 200,
+        backgroundColor: Colors.red,
+        loaderColor: Colors.yellow,
+      ),
+
+    );
+  }
+}
